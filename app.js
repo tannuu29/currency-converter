eca86fa30a2ff88d371ade75f198fc821801838b
Tanisha Nainwal <tanishanainwal.5@gmail..com>
2025-06-13 15:03:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const BASE_URL = "https://2024-03-06.currency-api.pages.dev/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select")
+const msg = document.querySelector(".msg");
+
+// for(code in countryList){
+//     console.log(code, countryList[code]);
+// }
+
+for (let select of dropdowns) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name == "from" && currCode == "USD") {
+            newOption.selected = "selected";
+        }
+        if (select.name == "to" && currCode == "INR") {
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);  
+    })
+}
+
+const updateFlag = (elem) => {
+    // console.log(elem);
+    let currCode = elem.value;
+    // console.log(currCode); 
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = elem.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+
+const updateExchangeRate = async () => {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal == "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+
+    // console.log(fromCurr.value, toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+
+        const from = fromCurr.value.toLowerCase();
+        const to = toCurr.value.toLowerCase();
+        const rate = data[from][to];
+        if (rate === undefined) {
+            throw new Error("Rate not found for selected currencies.");
+        }
+
+        let finalAmount = (amtVal * rate).toFixed(3);
+        //1USD = 82.8INR
+        msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`
+    }
+    catch (error) {
+        console.log("error fetching data")
+    }
+}
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+})
